bd48f582da3fab65b2568fcfc527d1d3e7bcca8c
Chaoran Lu <andyluloveusc@gmail.com>
2024-01-11 16:49:23 -0800

    test contact-us page

--- test/specs/contact-us.spec.js
+++ test/specs/contact-us.spec.js
@@ -1,21 +1,52 @@
 describe('webdriveruniversity - contact us page', () => {
     beforeEach(async () => {
         await browser.maximizeWindow();
+        await browser.url("/Contact-Us/contactus.html");
     });
     it('valid submission - submit all information', async() => {
-        await browser.url("Contact-Us/contactus.html");
-        await browser.pause(5000);
+        // Arrange 
         // first name
+        const firstName = await $('//*[@name="first_name"]');
         // last name
+        const lastName = await $('//*[@name="last_name"]');
         // email address
+        const email = await $('//*[@name="email"]');
         // message
+        const msg = await $('//*[@name="message"]');
         // submit button
+        const submitBtn = await $('//*[@value="SUBMIT"]');
+
+        // Act
+        await firstName.setValue("Andrew");
+        await lastName.setValue("Lu");
+        await email.setValue("clu2024@outlook.com");
+        await msg.setValue("Hello Webdriver.IO");
+        await submitBtn.click();
+
+        // Assert
+        const successfulSubmissionHeader = $('#contact_reply > h1');
+        await expect(successfulSubmissionHeader).toHaveText('Thank You for your Message!');
     });
 
-    it('invalid submission - dont submit all information', () => {
+    it.only('invalid submission - dont submit all information', async() => {
+        // Arrange
         // first name
+        const firstName = await $('//*[@name="first_name"]');
         // last name
+        const lastName = await $('//*[@name="last_name"]');
         // message
+        const msg = await $('//*[@name="message"]');
         // submit button
+        const submitBtn = await $('//*[@value="SUBMIT"]');
+
+        // Act
+        await firstName.setValue("Andrew");
+        await lastName.setValue("Lu");
+        await msg.setValue("Hello Webdriver.IO");
+        await submitBtn.click();
+
+        // Assert
+        const successfulSubmissionHeader = $('body');
+        await expect(successfulSubmissionHeader).toHaveTextContaining(['Error: all fields are required', 'Error: Invalid email address']);
     });
 });
