2305c3b5addb21a3c9c42ad21f1f6b8fe09f6b5a
Chaoran Lu <andyluloveusc@gmail.com>
2024-01-17 14:34:19 -0800

    POM - Real World Example - Part 4

--- test/specs/automation-test-store/add-items-to-basket.spec.js
+++ test/specs/automation-test-store/add-items-to-basket.spec.js
@@ -8,46 +8,6 @@ describe('add items to basket', () => {
 
         await HomePage.categoryMenuComponent.categoryMenuLink('Skincare')[1].click();
 
-        const skincareProducts_Header_Links = await SkinCarePage.itemComponent.itemHeaderLinks;
-
-        const itemPrices = [];
-
-        for(const header of skincareProducts_Header_Links) {
-            const tempHeaderText = await header.getText();
-            
-            if(tempHeaderText.toLowerCase() == "creme precieuse nuit 50ml" || 
-            tempHeaderText.toLowerCase() == "total moisture facial cream") {
-                const attr = await header.getAttribute("href");
-                const itemId = attr.split("id=").pop();
-                await $('//a[@data-id="' + itemId + '"]').click();
-
-                itemPrices.push(
-                    await $("//a[@data-id='" + itemId + "']/following-sibling::div/div[@class='pricenew']"
-                    + "| //a[@data-id='" + itemId + "']/following-sibling::div/div[@class='oneprice']").getText()
-                )
-            }
-            const formattedItemPrices = [];
-
-            itemPrices.forEach((price) => {
-                formattedItemPrices.push(price.replace("$", ""));
-            });
-
-            var itemsTotal = 0;
-            formattedItemPrices.forEach(price => itemsTotal += parseFloat(price));
-        }
-        
-        await $("//span[text()='Cart']").click();
-        await expect(browser).toHaveUrlContaining("checkout");
-
-        var tempShippingRate = await $("//span[text()='Flat Shipping Rate:']/../following-sibling::td").getText();
-        var shippingRate = tempShippingRate.replace('$', '');
-        itemsTotal = itemsTotal + parseFloat(shippingRate);
-
-        // extract cart total
-        var cartTotal = await $("//span[text()='Total:']/../following-sibling::td").getText();
-        cartTotal = cartTotal.replace('$', '');
-        expect(itemsTotal).toEqual(parseFloat(cartTotal));
-
-        await browser.pause(5000);
+        await SkinCarePage.addSpecificItems_ValidateTotal("creme precieuse nuit 50ml", "total moisture facial cream");
     });
 });
